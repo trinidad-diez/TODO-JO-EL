5753068a5241c51f28345f8c66370436fea37c0a
Tri <trinidad.diez.7e9@itb.cat>
2025-12-12 12:26:18 +0100

    Problema 7/50.

---
+++ Problemas/1/Quadrat.kt
@@ -0,0 +1,23 @@
+import java.util.Scanner
+
+/*
+    Entrada: numero (Int)
+    Sortida: numero * numero.
+ */
+
+fun main(){
+    // Crea un escàner per llegir l'entrada de l'usuari.
+    val scan : Scanner = Scanner (System.`in`)
+
+    // Emmagatzema els valors escanejats en variables.
+    var numero : Int = scan.nextInt()
+
+    // Crea una variable per a la suma dels dos nombres.
+    val area = numero * numero
+
+    // Imprimeix la suma dels dos números a la consola.
+    println(area)
+
+    // Tanca l'escàner.
+    scan.close()
+}
