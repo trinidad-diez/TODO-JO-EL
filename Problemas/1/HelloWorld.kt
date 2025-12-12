c4edf36e8adc7652e7b0e66ae03de59230285ced
Tri <trinidad.diez.7e9@itb.cat>
2025-12-12 11:57:07 +0100

    Problema 1/50.

---
+++ Problemas/1/HelloWorld.kt
@@ -0,0 +1,13 @@
+
+/*
+    Entrada: -
+    Sortida: Hello World
+ */
+
+fun main(){
+    // Crea una varuiable per al missatge.
+    val missatge = "Hello World"
+
+    // Imprimeix el missatge a la consola.
+    println(missatge)
+}
